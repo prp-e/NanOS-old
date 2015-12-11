90f486d56c8519bdd351224fbc0e7dbf75cd0588
Muhammadreza Haghiri <haghiri75@gmail.com>
2015-12-11 23:53:30 +0330

    Kernel Source, Some changes applied

--- kernel.c
+++ kernel.c
@@ -95,13 +95,13 @@ void terminal_writestring(const char* data){
   }
 }
 
-static inline void inb(unsigned short port){
+static inline short inb(unsigned short port){
  unsigned char value;
  asm volatile("inb %w1, %0" : "=a" (value) : "Nd" (port));
  return value;
 }
 
-static inline void outb(unsigned short port, unsigned char value){
+static inline short outb(unsigned short port, unsigned char value){
  asm volatile("outb %b0, %w1" : : "a" (value), "Nd" (port));
 }
 
