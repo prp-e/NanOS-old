f73510a037cd6dafcfa6340bf227b7e73f0ec30f
Muhammadreza Haghiri <haghiri75@gmail.com>
2015-12-12 00:08:01 +0330

    Keyboard entry function added!

--- kernel.c
+++ kernel.c
@@ -145,7 +145,22 @@ unsigned char scancode[128] =
     0,    /* All other keys are undefined */
 };
 
+char getScancode(){
+char c=0;
+do {
+if(inb(0x60)!=c)
+{
+c=inb(0x60);
+if(c>0)
+return c;
+}
+}while(1);
+}
 
+char getchar()
+{
+return scancode[getScancode()+1];
+}
 
 
 
